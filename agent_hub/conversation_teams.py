16f5c55e471aa1768da984ebb95f251b6153616a
TommyA8 <nengnoia8@gmail.com>
2025-08-22 16:57:45 +0700

    Edit help desk method

--- agent_hub/conversation_teams.py
+++ agent_hub/conversation_teams.py
@@ -4,6 +4,7 @@ from langgraph.graph import StateGraph, START, END
 
 # Model
 from model.state_model import MainState
+from utils.get_latest_question import get_latest_question
 
 class ConversationTeams:
     def __init__(self, model: ChatOllama):
@@ -17,16 +18,29 @@ class ConversationTeams:
         return g.compile()
     
     def help_desk(self, state: MainState):
+
+        question = get_latest_question(state)
+
         system = SystemMessage(content=(
-            "Your name is ChatCM.\n"
-            "You are a helpful, concise, and polite help desk for a CM system.\n"
-            "You can only answer questions related to Construction Management (CM), greetings and about yourself.\n"
-            "Always use prior conversation context if helpful. Keep answers SHORT."
-            "DO NOT Answer with prompts."
-        ))
+                "Your name is ChatCM."
+                "You are a helpful, concise, and polite help desk for a CM system."
+                "You are given a question from a user.\n"
+                # "Question: {question}\n"
+                "You can only to answer questions only related to Construction Management (CM), greetings and about yourself. Follwing instructions:\n"
+                "- Always use prior conversation context if helpful. Keep answers SHORT.\n"
+                "- You can answer questions in table format if needed.\n"
+                "- You can use database tables and columns as context.\n"
+                "- DO NOT Answer with prompts.\n"
+                "- DO NOT Answer with SQLQuery.\n"
+            ))
         
-        # invoke the chat model correctly with a list of messages
-        response = self.model.invoke(state["messages"] + [system])
+        try:
+            db_cntx = SystemMessage(content=f"Database Context:\n{state['relavant_context']}")
+            response = self.model.invoke(state["messages"] + [system] + [db_cntx])
+
+        except:
+            # invoke the chat model correctly with a list of messages
+            response = self.model.invoke(state["messages"] + [system])
 
         ai_msg = AIMessage(content=response.content)
         # ai_msg = AIMessage(content=response.content.split("</think>\n")[-1])
