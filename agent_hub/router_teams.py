f5441d8c816235dd8161dfd2d321dbb96f83c2f2
TommyA8 <nengnoia8@gmail.com>
2025-08-22 17:01:21 +0700

    Edit prompt and add tool names

--- agent_hub/router_teams.py
+++ agent_hub/router_teams.py
@@ -1,11 +1,14 @@
 from langchain_ollama import ChatOllama
-from langchain_core.messages import AIMessage
+from langchain_core.messages import AIMessage, SystemMessage
 from langgraph.graph import StateGraph, START, END
 
 # Model
 from model.state_model import RouterState, RoutingDecision
 # Utils
 from utils.get_latest_question import get_latest_question
+from model.state_model import Tools
+
+tools = [tool.value for tool in Tools]
 
 class RouterTeams:
     def __init__(self, model: ChatOllama):
@@ -20,21 +23,25 @@ class RouterTeams:
 
     def tool_classification(self, state: RouterState):
         prompt = """
-        You are a Costruction Management expert. You know end to end Construction Management process.
-        Analyze the user query below and determine its Available Tools with deeply reason.
+        You are a Costruction Management expert.\n
+        You are an expert in Construction Management\n.
+        Analyze the user query below and determine its Available Construction Management 'Tools' with deeply reason.
 
-        question:
-        {question}
+        If you are not sure about the tools, please respond with 'Unknown'.
+
+        Tools:
+        {tools}
         """
 
         agent = self.model.with_structured_output(RoutingDecision)
 
         question = get_latest_question(state)
-        decision = agent.invoke(prompt.format(question=question[-1].content))
+        # decision = agent.invoke(prompt.format(question=question[-1].content))
+        decision = agent.invoke(state['messages'] + [SystemMessage(content=prompt.format(tools=tools))])
 
         return {
             "messages": [AIMessage(content=f"LLM Router decided: {decision.tool.value}")],
             "tool": decision.tool,
-            "tool_selected_reason": decision.tool_selected_reason,
+            # "tool_selected_reason": decision.tool_selected_reason,
         }
 
