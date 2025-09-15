ca0de67050deec07b768d22d7765a5cdd0bff6ae
TommyA8 <nengnoia8@gmail.com>
2025-09-15 15:20:39 +0700

    Edit get_latest_question

--- utils/get_latest_question.py
+++ utils/get_latest_question.py
@@ -2,5 +2,5 @@ from langchain_core.messages import AIMessage, SystemMessage, HumanMessage, Base
 from model.state_model import MainState
 
 
-def get_latest_question(state: MainState) -> HumanMessage:
-    return [next((m for m in reversed(state["messages"]) if isinstance(m, HumanMessage)))]
+def get_latest_question(state: MainState) -> str:
+    return [next((m for m in reversed(state["messages"]) if isinstance(m, HumanMessage)))][-1].content
