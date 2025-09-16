d2fb847ab1aae35c53e7d38850f4acf1f86397b4
TommyA8 <nengnoia8@gmail.com>
2025-09-16 14:31:09 +0700

    Update prompt

--- agents/general_assistant.py
+++ agents/general_assistant.py
@@ -10,9 +10,13 @@ class GeneralAssistant:
     def __init__(self, model: ChatOllama):
         self.model = model
         self.prompt = (
+            "Your name is ChatCM.\n"
+            "You are a concise, polite, and professional help desk assistant for a Construction Management (CM) system.\n"
             "You're a friendly assistant and your goal is to answer general questions.\n"
             "If the user asks something out of scope, politely decline and remind them of the supported topics.\n"
             "Keep answers concise, clear, and polite.\n"
+            "You must use the provided **CHAT HISTORY** to infer intent."
+            "Question:\n{question}\n"
         )
 
     def build(self, checkpointer=None):
@@ -23,8 +27,9 @@ class GeneralAssistant:
         g.add_edge("general", END)
         return g.compile(checkpointer) if checkpointer is not None else g.compile()
     
-    def general_assistant(self, state: MessagesState):
-        res = self.model.invoke([SystemMessage(content=self.prompt)] + state['messages'])
+    def general_assistant(self, state: MessagesState) -> MessagesState:
+        prompt = self.prompt.format(question=get_latest_question(state))
+        res = self.model.invoke([SystemMessage(content=prompt)] + state['messages'])
 
         # Ensure output is an AIMessage or at least has 'content'
         ai_message = res if isinstance(res, AIMessage) else AIMessage(content=res['messages'][-1].content)
